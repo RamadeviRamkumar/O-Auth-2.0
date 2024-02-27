6711699f576c55f5579e54cc98138ffc15d355aa
RamadeviRamkumar <ramdevcse25@gmail.com>
2024-02-27 18:35:15 +0530

    update

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(
-	cors({
-		origin: "http://localhost:5000",
-		methods: "GET,POST,PUT,DELETE",
-		credentials: true,
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: "http://localhost:5000",
+// 		methods: "GET,POST,PUT,DELETE",
+// 		credentials: true,
+// 	})
+// );
+app.use(cors());
 
 app.use("/auth", authRoute);
 
